4b3c3c58265fdeaade45dd6520d96a500e33ed0b
Daniel Azuma <dazuma@google.com>
2023-05-05 12:24:06 -0700

    chore: Set up release system (#20)

---
+++ .kokoro/release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+# Install gems in the user directory because the default install directory
+# is in a read-only location.
+export GEM_HOME=$HOME/.gem
+export PATH=$GEM_HOME/bin:$PATH
+
+gem install --no-document toys
+toys release install-python-tools -v
+python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+toys release perform -v < /dev/null
